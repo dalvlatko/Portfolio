80906aec2fce4b1b94ff8955d382c48044a38fdb
dalvlatko <vladimir.icovski@gmail.com>
2021-04-18 17:39:00 -0400

    Added NavBar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,31 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
-        <nav>
-          <NavLink to="/" exact>
+    <header className="bg-blue-100">
+      <div className="container">
+        <nav className="flex justify-center">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="title inline-flex items-center py-6 px-3 mr-4 hover:text-black-500 font-bold"
+          >
             Vlatko
           </NavLink>
-          <NavLink to="/project">Projects</NavLink>
-          <NavLink to="/about">About Me</NavLink>
+          <NavLink
+            to="/project"
+            activeClassName="text-white"
+            className="inline-flex items-center py-6 px-3 mr-4 hover:text-black-500 font-bold"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            activeClassName="text-white"
+            className="inline-flex items-center py-6 px-3 mr-4 hover:text-black-500 font-bold"
+          >
+            About Me
+          </NavLink>
         </nav>
       </div>
     </header>
