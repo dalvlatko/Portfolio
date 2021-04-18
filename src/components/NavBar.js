666d99e9ffefade62389fb5064866420851ff3f2
dalvlatko <vladimir.icovski@gmail.com>
2021-04-18 17:25:43 -0400

    Added NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/" exact>
+            Vlatko
+          </NavLink>
+          <NavLink to="/project">Projects</NavLink>
+          <NavLink to="/about">About Me</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
