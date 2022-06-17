98ee53c0d00ce335195f4b6022daac497e90aa33
dalvlatko <vlatko@Vladimirs-MacBook-Pro.local>
2022-06-16 21:59:42 -0400

    Fixed About error not loading

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import sanityClient from "../client.js";
 
 export default function About() {
   const [aboutData, setAboutData] = useState(null);
+
   useEffect(() => {
     sanityClient
       .fetch(
@@ -12,20 +13,22 @@ export default function About() {
         bio
       }`
       )
-      .then((data) => setAboutData(data))
+      .then((data) => setAboutData(data[0]))
       .catch(console.error);
   }, []);
+
+  if (!aboutData) return <div>Loading...</div>;
 
   return (
     <section className="pt-12 pb-12 px-8">
         <h1 className="flex justify-center text-6xl text-black font-bold leading-none lg:leading-snug home-name">
-          {aboutData[0].name}
+          {aboutData.name}
         </h1>
         <h2 className="flex justify-center">
           This is the about section
         </h2>
         <p>
-          {aboutData[0].bio}
+          {aboutData.bio}
         </p>
     </section>
   )
