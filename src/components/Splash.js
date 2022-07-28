3f22e4cfad217cd0b3b31ebcec27abb05ca329e0
dalvlatko <vladimir.icovski@gmail.com>
2022-07-28 08:51:37 -0400

    Cleaned up styling from Splash text

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,15 +3,15 @@ import { SocialIcon } from "react-social-icons";
 
 export default function Splash() {
   return (
-    <section className="flex-column justify-center pt-12 pb-12 px-8 w-screen h-screen bg-blue-100">
-      <div className="flex-column text-2xl text-black">
-        <h1 className=" text-5xl text-black font-bold leading-none lg:leading-snug home-name">
+    <section className="flex-column pt-12 pb-12 px-8 w-screen h-screen bg-blue-100">
+      <div className="flex-column text-right pb-80">
+        <h1 className="text-5xl text-black font-bold leading-none lg:leading-snug home-name">
           Hello, I'm Vladimir Icovski
         </h1>
-        <h2>Web Developer</h2>
-        <h2>Chemist</h2>
+        <h2 className="text-2xl">Web Developer</h2>
+        <h2 className="text-2xl">Chemist</h2>
       </div>
-      <div className="">
+      <div className="self-end">
         <SocialIcon
           url="https://instagram.com/dalvlatko"
           className="m-3"
