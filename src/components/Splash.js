04aec28acaa55275b8b76e85ac5547b516ebdd8c
dalvlatko <vladimir.icovski@gmail.com>
2022-07-28 04:31:18 -0400

    Removed styling from Splash text

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,23 +3,27 @@ import { SocialIcon } from "react-social-icons";
 
 export default function Splash() {
   return (
-    <section className="pt-12 pb-12 px-8 w-screen h-screen bg-blue-100">
-      <h1 className="flex justify-center text-5xl text-black font-bold leading-none lg:leading-snug home-name">
-        Hello, I'm Vladimir Icovski
-      </h1>
-      <div className="flex justify-center">
+    <section className="flex-column justify-center pt-12 pb-12 px-8 w-screen h-screen bg-blue-100">
+      <div className="flex-column text-2xl text-black">
+        <h1 className=" text-5xl text-black font-bold leading-none lg:leading-snug home-name">
+          Hello, I'm Vladimir Icovski
+        </h1>
+        <h2>Web Developer</h2>
+        <h2>Chemist</h2>
+      </div>
+      <div className="">
         <SocialIcon
           url="https://instagram.com/dalvlatko"
           className="m-3"
           target="_blank"
         />
         <SocialIcon
-          url="https://twitter.com/dalvlatko"
+          url="https://github.com/dalvlatko"
           className="m-3"
           target="_blank"
         />
         <SocialIcon
-          url="https://github.com/dalvlatko"
+          url="https://linkedin.com/in/vladimir-icovski"
           className="m-3"
           target="_blank"
         />
