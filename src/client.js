3f46058d6134e9fb57393d4c8abfc3f7ebb50583
dalvlatko <vladimir.icovski@gmail.com>
2022-08-05 18:31:04 -0400

    Use sanity CDN

--- src/client.js
+++ src/client.js
@@ -3,4 +3,7 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "1i41jkhj",
   dataset: "production",
+  apiVersion: "2022-08-05", // use current UTC date - see "specifying API version"!
+  token: "", // or leave blank for unauthenticated usage
+  useCdn: true, // `false` if you want to ensure fresh data
 });
