7ede4c3e39baaf1e46f9f09bd09f5d74ed4fbee7
dalvlatko <vladimir.icovski@gmail.com>
2022-08-10 22:03:13 -0400

    Adjusted favicon, padding on bgtext

--- src/components/BackgroundText.js
+++ src/components/BackgroundText.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function BackgroundText({ text, reps, bgTextColor, position }) {
   return (
     <div
-      className={`z-0 text-[5rem] sm:text-[12rem] absolute select-none overflow-hidden break-all font-black ${bgTextColor} leading-none ${position}`}
+      className={`z-0 text-[5rem] sm:text-[12rem] absolute select-none overflow-hidden break-all font-black ${bgTextColor} leading-none pl-3 sm:pl-12 ${position}`}
       data-nosnippet
     >
       {text.repeat(reps)}
