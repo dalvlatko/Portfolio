ed8ebdb6aac8de15b040e6394b539edaaaf3d2c7
dalvlatko <vladimir.icovski@gmail.com>
2022-08-14 16:50:06 -0400

    Added archive page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Post from "./Post";
 import Footer from "./Footer";
-import BackgroundText from "./BackgroundText.js";
 
 export default function Home() {
   return (
