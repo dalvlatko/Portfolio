06d896e8f1005075bdb7bfc2cee64c958f66cab5
dalvlatko <vladimir.icovski@gmail.com>
2024-06-22 14:42:11 -0400

    Changed projects link in navbar to jump down page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,26 +8,26 @@ export default function NavBar() {
         to="/"
         exact
         activeClassName="text-mintcream"
-        className="inline-flex items-center py-6 px-3 mr-4 text-gunmetal hover:text-crayola"
+        className="font-extrabold inline-flex items-center py-6 px-3 xl:px-20 text-gunmetal hover:text-crayola"
       >
         Home
       </NavLink>
       <a
         href="#About"
-        className="inline-flex items-center py-6 px-3 mr-4 text-gunmetal hover:text-crayola"
+        className="font-extrabold inline-flex items-center py-6 px-3 xl:px-20 text-gunmetal hover:text-crayola"
       >
         About
       </a>
-      <NavLink
-        to="/#Projects"
-        className="inline-flex items-center py-6 px-3 mr-4 text-gunmetal hover:text-crayola"
+      <a
+        href="#Projects"
+        className="font-extrabold inline-flex items-center py-6 px-3 xl:px-20 text-gunmetal hover:text-crayola"
       >
         Projects
-      </NavLink>
+      </a>
       <NavLink
         to="/blog"
         activeClassName="text-mintcream"
-        className="inline-flex items-center py-6 px-3 mr-4 text-gunmetal hover:text-crayola"
+        className="font-extrabold inline-flex items-center py-6 px-3 xl:px-20 text-gunmetal hover:text-crayola"
       >
         Blog
       </NavLink>
